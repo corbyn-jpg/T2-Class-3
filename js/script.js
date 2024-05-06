510f7ac64ce0c54bf839d8468ef1411770086406
corbyn-jpg <159898463+corbyn-jpg@users.noreply.github.com>
2024-05-06 15:53:56 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -10,3 +10,18 @@ for(let i = 0; i < 3; i++){
     let sent = "My name is Corbyn";
     console.log(sent);
 }
+
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+let even = [];
+let odd = [];
+
+for(let i = 0; i < numbers.length; i++){
+    if(numbers [i] %2 != 1){
+        even.push(numbers[i]);
+    }else{
+        odd.push(numbers[i]);
+    }
+}
+
+console.log(even);
+console.log(odd);
